797ea0bfaf19779a6872871ae4b91166e5aaf311
Tapdig Guliyev <tapdigguliyev@gmail.com>
2022-09-14 13:30:55 +0400

    Total run statistics and SettingsFragment done, slight layout updates

--- app/src/main/java/com/example/runningapp/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/SettingsFragment.kt
@@ -1,18 +1,78 @@
 package com.example.runningapp.ui.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.example.runningapp.R
 import com.example.runningapp.databinding.FragmentSettingsBinding
+import com.example.runningapp.other.Constants.KEY_NAME
+import com.example.runningapp.other.Constants.KEY_WEIGHT
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment() {
 
     private lateinit var binding: FragmentSettingsBinding
 
+    @Inject
+    lateinit var sharedPref: SharedPreferences
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadFieldsFromSharedPref()
+
+        binding.btnApplyChanges.setOnClickListener {
+            it.hideKeyboard()
+            val success = applyChangesToSharedPref()
+            if (success) {
+                Snackbar.make(view, "Saved changes", Snackbar.LENGTH_SHORT).show()
+            } else {
+                Snackbar.make(view, "Please enter all the fields", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loadFieldsFromSharedPref() {
+        val name = sharedPref.getString(KEY_NAME, "") ?: ""
+        val weight = sharedPref.getFloat(KEY_WEIGHT, 80f)
+
+        binding.etName.setText(name)
+        binding.etWeight.setText(weight.toString())
+    }
+
+    private fun applyChangesToSharedPref(): Boolean {
+        val name = binding.etName.text.toString()
+        val weight = binding.etWeight.text.toString()
+        if (name.isEmpty() || weight.isEmpty()) {
+            return false
+        }
+
+        sharedPref.edit()
+            .putString(KEY_NAME, name)
+            .putFloat(KEY_WEIGHT, weight.toFloat())
+            .apply()
+
+        val toolbarText = "Let's go, $name!"
+        requireActivity().findViewById<TextView>(R.id.tvToolbarTitle).text = toolbarText
+        return true
+    }
+
+    private fun View.hideKeyboard() {
+        val inputManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(windowToken, 0)
     }
 }
--- app/src/main/java/com/example/runningapp/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/example/runningapp/ui/fragments/StatisticsFragment.kt
@@ -7,8 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.runningapp.databinding.FragmentStatisticsBinding
+import com.example.runningapp.other.TrackingUtility
 import com.example.runningapp.ui.viewmodels.StatisticsViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.round
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment() {
@@ -19,5 +21,41 @@ class StatisticsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentStatisticsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        subscribeToObservers()
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.totalTimeRun.observe(viewLifecycleOwner) {
+            it?.let {
+                val totalTimeRun = TrackingUtility.getFormattedStopWatchTime(it)
+                binding.tvTotalTime.text = totalTimeRun
+            }
+        }
+        viewModel.totalDistance.observe(viewLifecycleOwner) {
+            it?.let {
+                val km = it / 1000f
+                val totalDistance = round(km * 10f) / 10f
+                val totalDistanceString = "${totalDistance}km"
+                binding.tvTotalDistance.text = totalDistanceString
+            }
+        }
+        viewModel.totalAvgSpeed.observe(viewLifecycleOwner) {
+            it?.let {
+                val avgSpeed = round(it * 10f) / 10f
+                val avgSpeedString = "${avgSpeed}km/h"
+                binding.tvAverageSpeed.text = avgSpeedString
+            }
+        }
+        viewModel.totalCaloriesBurned.observe(viewLifecycleOwner) {
+            it?.let {
+                val totalCalories = it
+                val totalCaloriesString = "${totalCalories}kCal"
+                binding.tvTotalCalories.text = totalCaloriesString
+            }
+        }
     }
 }
